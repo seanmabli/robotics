c274aa5153744cfde5d3fff3bf70ed7f67545bf2
SeanMabli <smdrone1@gmail.com>
2021-11-01 20:11:31 -0400

    Add 20 lines of code (distance to subsolar point)

---
+++ DistanceToSubsolarPoint.py
@@ -0,0 +1,16 @@
+import ephem
+import geocoder
+import math
+
+greenwich = ephem.Observer()
+sun = ephem.Sun(greenwich)
+SunLongitude = 57.2958 * (sun.ra - greenwich.sidereal_time())
+SunLongitude += 360 if SunLongitude < -180 else 0
+SunLongitude -= 360 if SunLongitude > 180 else 0
+SunLatitude = 57.2958 * sun.dec
+
+MyLongitude, MyLatitude = geocoder.ip('me').latlng[0], geocoder.ip('me').latlng[1]
+
+Distance = ((SunLongitude - MyLongitude) ** 2 + (SunLatitude - MyLatitude) ** 2) ** 0.5
+
+print("The Distance From Us And The SubSolar Point Is:", (Distance / 360) * math.pi * 2 * 6371, "Km")
