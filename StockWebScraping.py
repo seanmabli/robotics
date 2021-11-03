5103bb224227ff7b1c80ca5d2ccadb8a6f02e335
SeanMabli <smdrone1@gmail.com>
2021-11-03 18:45:43 -0400

    fix stock price api and delete unwanted files

--- StockWebScraping.py
+++ StockWebScraping.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = requests.get('https://finance.yahoo.com/quote/%5EGSPC/')
-soup = BeautifulSoup(url.text, features="html.parser")
+response = requests.get('https://finance.yahoo.com/quote/TSLA?')
+ 
+soup = BeautifulSoup(response.text, 'html.parser')
 
-print(soup.find_all("div", {'class': 'Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)'}))
+price = float(soup.find(class_='Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)').get_text().replace(',', ''))
+print(price)
